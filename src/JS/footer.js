0382b48edf9c34780ae0c4fad2761ce27894a600
Torres Leo <leo.torres@etu.umontpellier.fr>
2024-04-04 09:49:15 +0200

    refactor(): generalisation du footer

---
+++ src/JS/footer.js
@@ -0,0 +1,18 @@
+let Footer = '<p id="signature">&copy; 2023 Torres Léo - Tous droits réservés</p>\n' +
+    '    <div id="RS">\n' +
+    '        <a href="https://www.instagram.com/le0_trs/">\n' +
+    '            <img src="../ressource/image/general/instagram.png" alt="insta" class="reseau">\n' +
+    '        </a>\n' +
+    '        <a href="https://www.facebook.com/profile.php?id=100017006473298">\n' +
+    '            <img src="../ressource/image/general/facebook.png" alt="facebook" class="reseau">\n' +
+    '        </a>\n' +
+    '        <a href="https://www.linkedin.com/in/leo-torres-804687264/">\n' +
+    '            <img src="../ressource/image/general/linkedin.png" alt="linkedin" class="reseau">\n' +
+    '        </a>\n' +
+    '        <a href="https://github.com/laflut3">\n' +
+    '            <img src="../ressource/image/general/github.png" alt="github" class="reseau">\n' +
+    '        </a>\n' +
+    '    </div>';
+
+const footer = document.getElementById("footer");
+footer.innerHTML += Footer;
